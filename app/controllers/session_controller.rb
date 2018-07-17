8b6f3ac117964b47988ba9a03fa4ff988b75816f
Bryan Hooper <bryan@lindsayfarm.net>
2018-07-17 11:00:21 -0400

    Added basic validation for the user when he/she registers a new account.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -19,15 +19,15 @@ class SessionController < ApplicationController
   end
 
   post '/registration' do
-    if !logged_in?
-      @user = User.new(username: params[:username], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], email: params[:email])
+    if !logged_in? && User.new(username: params[:username], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], email: params[:email]).valid?
+        @user = User.new(username: params[:username], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], email: params[:email])
       @user.save
       session[:email] = params[:email]
       @my_surveys = @user.surveys
       @all_surveys = Survey.all
       redirect '/surveys'
     else
-      redirect '/'
+      redirect :'/'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  validates :username, :email, presence: { message: "You must provide a username and email." }
+  validates :username, :email, uniqueness: { message: "That username and/or email is alreday taken." } 
+  
   has_secure_password
 
   has_many :surveys
